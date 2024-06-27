2f528f07c50f837948e61371624bce7f7aaa3c52
aylees <sutherlin.rylee@gmail.com>
2024-06-26 23:53:38 -0500

    Part 2 With Template Literals Issue

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,34 +12,39 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = "Who was the first American woman in space? ";
-let correctAnswers = "Sally Ride";
-let candidateAnswers = "";
+let questions = ["Who was the first American woman in space? ", 
+"True or false: 5 kilometer == 5000 meters? ", 
+"(5 + 3)/2 * 10 = ? ", 
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName= input.question("Please enter your name here: ");
+candidateName = input.question("Please enter your name here: ");
 }
 
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-question = input.question(question);
-
-candidateAnswer = question
+for (let i = 0; i < questions.length; i++) {
+  candidateAnswers.push(input.question(questions[i]));
+}
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  console.log("Great Job! You're correct!");
-} else {
-  console.log("Oh no! You're incorrect, please try again!");
+for (let i = 0; i < candidateAnswers.length; i++)
+{
+  if (candidateAnswers[i] == correctAnswers[i]) {
+    console.log('Great job! Your answer is correct: ${candidateAnswers[i]}')
+  } else {
+    console.log('Oh no! Your answer is wrong: ${candidateAnswers[i]}');
+  }
 }
-
-
-
+// the template literal ${candidateAnswers[1]} is not working properly, I do need to comeback to this, but want to turn in waht I have
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
